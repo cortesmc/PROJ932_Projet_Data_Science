17aec0cedcdff72bce8be050ae8cd3d8e700c023
cortesmc <cacm@cassini.group>
2025-02-11 14:52:37 +0100

    gephi

---
+++ proj932_graph/graph_dashboard/middleware/cors_static_middleware.py
@@ -0,0 +1,11 @@
+class CorsStaticMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if request.path.startswith('/static/'):
+            response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+            response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response
